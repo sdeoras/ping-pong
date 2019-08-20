5fc363636bc1bc243a35e832a920637b9c1e1dcd
Saurabh Deoras <saurabh@diamanti.com>
2019-08-20 14:02:33 -0700

    using buildah to build containers
    Signed-off-by: Saurabh Deoras <saurabh@diamanti.com>

---
+++ build/build-client.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# environment variables
+# system
+export OS="linux"
+export ARCH="amd64"
+export COMPONENT="client"
+export CONTAINER_HOSTNAME="ping-pong-${COMPONENT}"
+# config
+export DEFAULT_IMAGE_BASENAME="docker.io/sdeoras/${CONTAINER_HOSTNAME}"
+
+# create a new tmp container from centos base and capture id in CTR.
+CTR=$(buildah from docker.io/library/alpine:latest)
+TAG=`git rev-parse HEAD | cut -c 1-8`
+STATE=`[[ -n $(git status -s) ]] || echo -n 'clean'`
+if [[ "${STATE}" = "" ]]; then
+    STATE="dirty"
+fi
+IMG="${DEFAULT_IMAGE_BASENAME}:${TAG}-${STATE}"
+echo "building container image: ${IMG}"
+
+buildah config --os="linux" --arch="amd64" ${CTR}
+buildah config --created-by "buildah" ${CTR}
+# currently unsupported commands, but should be added in future
+# buildah config --hostname ${CONTAINER_HOSTNAME}
+
+# configure environment variables for the container runtime.
+buildah config -e OS=${OS} ${CTR}
+buildah config -e ARCH=${ARCH} ${CTR}
+
+go build -o ping-pong-${COMPONENT} github.com/sdeoras/ping-pong/${COMPONENT}/cmd/src/ping-pong-${COMPONENT}
+buildah copy ${CTR} ping-pong-${COMPONENT} /
+rm -rf ping-pong-${COMPONENT}
+
+buildah run ${CTR} -- mkdir /lib64 
+buildah run ${CTR} -- ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
+
+# commit working container to a container image and remove working container
+buildah commit --rm ${CTR} ${IMG}
+
+# update yaml file
+sed -e "s/{{.Image}}/docker.io\/sdeoras\/ping-pong-${COMPONENT}:${TAG}-${STATE}/g" ${COMPONENT}.yaml > ../kubernetes-manifests/${COMPONENT}.yaml
+
+echo "====================================="
+echo "built container image ${IMG}"
+echo "====================================="
+
--- go.mod
+++ go.mod
@@ -3,11 +3,22 @@ module github.com/sdeoras/ping-pong
 go 1.12
 
 require (
-	github.com/golang/protobuf v1.3.1
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/golang/protobuf v1.3.2
+	github.com/magiconair/properties v1.8.1 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/spf13/cobra v0.0.3
-	github.com/spf13/viper v1.3.2
-	golang.org/x/net v0.0.0-20190514140710-3ec191127204 // indirect
-	google.golang.org/grpc v1.20.1
+	github.com/pelletier/go-toml v1.4.0 // indirect
+	github.com/spf13/afero v1.2.2 // indirect
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/viper v1.4.0
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
+	golang.org/x/sys v0.0.0-20190813064441-fde4db37ae7a // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55 // indirect
+	google.golang.org/grpc v1.23.0
 )
+
+// security fix: https://groups.google.com/forum/#!msg/golang-nuts/fCQWxqxP8aA/DbI5QZnMAAAJ
+replace golang.org/x/net/http2 => golang.org/x/net/http2 v0.0.0-20190813141303-74dc4d7220e7
